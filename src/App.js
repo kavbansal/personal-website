8a37cb20c21923176a293bd78dc96acb891cce72
Kavan Bansal <kavbansal1@gmail.com>
2021-01-21 21:50:21 -0500

    Fixed bug with landing page

--- src/App.js
+++ src/App.js
@@ -6,42 +6,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
        <div className="App">
       <Router>
         <Navigation />
         <Switch>
           <Route path="/" exact component={() => <About />} />
+          <Route path="/personal-website" exact component={() => <About />} />
           <Route path="/hobbies" exact component={() => <Hobbies />} />
         </Switch>
       </Router>
-    </div>
-{/*       
-      <header className="App-header">
-        
-        <About></About>
-        <p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/kavanbansal/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        &nbsp;&nbsp;
-        <a
-          className="App-link"
-          href="https://github.com/kavbansal"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-       */}
     </div>
   );
 }
